73c82debf699236ced60106848113d4422c1a451
Jordan Booker <jordan.booker@avanade.com>
2021-04-15 16:35:53 -0400

    Lesson 3

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -8,6 +9,16 @@ namespace GradeBook
         {
             if (args.Length > 0)
             {
+                var grades = new List<double>() { 82.1, 10.3, 91.2 };
+                grades.Add(56.1);
+                var result = 0.0;
+                foreach(double grade in grades)
+                {
+                    result += grade;
+                }
+                result /= grades.Count;
+
+                Console.WriteLine($"The average grade is {result:N1}");
 
                 Console.WriteLine($"Hello {args[0]}!");
 
