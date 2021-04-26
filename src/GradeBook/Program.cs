ab241d4a988f8ed2d92fac2ba251a85096a07b7b
Jordan Booker <jordan.m.booker@gmail.com>
2021-04-26 16:48:06 -0400

    Lesson 3

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,26 +7,20 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
+            var book = new Book();
+            book.AddGrade(89.1);
+
+            var grades = new List<double>() { 82.1, 10.3, 91.2 };
+            grades.Add(56.1);
+            var result = 0.0;
+            foreach (double grade in grades)
             {
-                var grades = new List<double>() { 82.1, 10.3, 91.2 };
-                grades.Add(56.1);
-                var result = 0.0;
-                foreach(double grade in grades)
-                {
-                    result += grade;
-                }
-                result /= grades.Count;
-
-                Console.WriteLine($"The average grade is {result:N1}");
-
-                Console.WriteLine($"Hello {args[0]}!");
-
+                result += grade;
             }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+
         }
     }
 }
